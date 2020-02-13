915ead82e0e6ca436f42a5ab4712a7a4e76161ac
Paweł Kochański <pawkoch98@gmail.com>
2020-02-14 00:55:22 +0100

    1. First setup of the Environment

---
+++ src/user/models/user.model.ts
@@ -0,0 +1,24 @@
+import { BaseModel, schemaOptions } from '../../shared/base.model';
+import { UserRole } from './user-role.enum';
+import { ModelType, prop } from 'typegoose';
+
+export class User extends BaseModel{
+  @prop({required: [true, 'Username is required'], minlength: [6, 'Must be at least 6 characters'], unique: true})
+  username: string;
+  @prop({required: [true, 'Username is required'], minlength: [6, 'Must be at least 6 characters']})
+  password: string;
+  @prop({enum: UserRole})
+  role?: UserRole;
+  @prop()
+  emial: string;
+
+
+  static get model() : ModelType<User>{
+    return new User().getModelForClass(User,{ schemaOptions })
+  }
+
+  static get modelName(): string{
+    return this.model.modelName;
+  }
+
+}
